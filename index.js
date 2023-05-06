0b9d0a85d19a6ab24c341e7dd5204bbb846f8573
le minh tri <leminhtri886@gmail.com>
2023-05-07 05:00:45 +0700

    Update

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
+
 const app = express();
 require("dotenv").config;
 
 const productRoute = require("./routes/product.route");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(express.static("public"));
+// call route
 productRoute(app);
+// port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Server running");
