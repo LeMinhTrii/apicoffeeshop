790b8c92c35be4aa138819d173bfb95c8afee17a
le minh tri <leminhtri886@gmail.com>
2023-05-07 20:33:24 +0700

    Update controllers

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,11 +1,11 @@
 module.exports = (app) => {
-    const ProductControler = require("../controllers/ProductControler");
-    app.route("/product").get(ProductControler.get).post(ProductControler.store);
+  const ProductControler = require("../controllers/ProductControler");
+  app.route("/product").get(ProductControler.get).post(ProductControler.store);
 
-    app
-        .route("/product/:id")
-        .get(ProductControler.detail)
-        .put(ProductControler.update)
-        .patch(ProductControler.update)
-        .delete(ProductControler.delete);
+  app
+    .route("/product/:id")
+    .get(ProductControler.detail)
+    .put(ProductControler.update)
+    .patch(ProductControler.update)
+    .delete(ProductControler.delete);
 };
