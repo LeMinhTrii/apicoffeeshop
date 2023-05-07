92af722221bd67783c1ad415711b2dfea69e32c0
le minh tri <leminhtri886@gmail.com>
2023-05-07 15:34:37 +0700

    Update vercel

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
-const path = require("path");
 const app = express();
 require("dotenv").config;
 
@@ -8,11 +7,6 @@ const productRoute = require("./routes/product.route");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(fileUpload());
-
-// Cấu hình express-fileupload
-
-// Cấu hình thư mục public cho Vercel
-app.use(express.static(path.join(__dirname, "public")));
 
 // call route
 productRoute(app);
