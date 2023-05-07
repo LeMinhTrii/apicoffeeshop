1ea56c13aebd4de7b9c7b3309bff1ec9cd1ce8c2
le minh tri <leminhtri886@gmail.com>
2023-05-08 04:15:47 +0700

    Update index

--- index.js
+++ index.js
@@ -5,27 +5,16 @@ const cors = require("cors");
 require("dotenv").config();
 const productRoute = require("./routes/product.route");
 
-app.use(
-  cors({
-    origin: "https://node-brown-chi.vercel.app/", // Thay đổi tên miền của ứng dụng của bạn
-    allowedHeaders: ["Content-Type", "Authorization"],
-    methods: "GET,PUT,POST,DELETE",
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  })
-);
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://node-brown-chi.vercel.app/"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // tăng giới hạn kích thước dữ liệu cho form-data
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
