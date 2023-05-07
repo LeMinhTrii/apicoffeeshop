915769c7aa88cb1261793f831430ff153cda945e
le minh tri <leminhtri886@gmail.com>
2023-05-07 15:33:58 +0700

    Update vercel

--- index.js
+++ index.js
@@ -7,19 +7,9 @@ require("dotenv").config;
 const productRoute = require("./routes/product.route");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(fileUpload());
+app.use(fileUpload());
 
 // Cấu hình express-fileupload
-app.use(
-  fileUpload({
-    createParentPath: true,
-    limits: { fileSize: 50 * 1024 * 1024 },
-    abortOnLimit: true,
-    useTempFiles: true,
-    tempFileDir: "/tmp/",
-    debug: true,
-  })
-);
 
 // Cấu hình thư mục public cho Vercel
 app.use(express.static(path.join(__dirname, "public")));
