62446f56fbdcf03fbe26f3df9f7d2d189bef0eda
le minh tri <leminhtri886@gmail.com>
2023-05-08 04:41:47 +0700

    Update index

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const productRoute = require("./routes/product.route");
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://127.0.0.1:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
