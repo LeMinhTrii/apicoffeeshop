0a87fd89fcc5685a8e5b006d8b97c76db3743bf0
le minh tri <leminhtri886@gmail.com>
2023-05-08 23:43:03 +0700

    Update1

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const productRoute = require("./routes/product.route");
 
 // cors
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
