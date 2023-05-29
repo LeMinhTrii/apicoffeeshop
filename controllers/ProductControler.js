78539d884f982d827f21897c73ae7c3dd68a5854
le minh tri <leminhtri886@gmail.com>
2023-05-30 04:35:41 +0700

    UpdateComment

--- controllers/ProductControler.js
+++ controllers/ProductControler.js
@@ -14,8 +14,8 @@ const productController = {
       }
       const data = {
         product: response,
-        count: response.length,
-        totalPage: Math.ceil(response.length / 8),
+        count: response.length || 0,
+        totalPage: Math.ceil(response.length || 0 / 8),
         perpage: 8,
       };
       res.status(200).json(data);
