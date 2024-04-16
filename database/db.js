55b91861a1f9e9753e7f207eaf1d1d2088c3c491
Lê Minh Trí <107636016+LeMinhTrii@users.noreply.github.com>
2024-04-16 23:36:25 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -13,10 +13,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "103.245.246.146",
-  user: process.env.DB_USER || "leminhtri",
-  password: process.env.DB_PASS || "qwer1234",
-  database: process.env.DB_NAME || "coffeapp",
+  host: process.env.DB_HOST || "bbv4cpycpurjnwengapg-mysql.services.clever-cloud.com",
+  user: process.env.DB_USER || "us0sdjqgpgxwq1tc",
+  password: process.env.DB_PASS || "gkiqMSJaUaQTvN6dbxtY",
+  database: process.env.DB_NAME || "bbv4cpycpurjnwengapg",
   port: 3306,
 });
 
